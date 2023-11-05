5cf6bd679617c2391a49cb81ac4df6a74426cb7b
Phoenix Pixel <camdengenta@gmail.com>
2023-11-04 20:56:40 -0500

    Moss Ball

---
+++ Scripts/moss2Scripts/moss22.gd
@@ -0,0 +1,21 @@
+@tool
+extends MeshInstance3D
+
+@export var update = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	passShaderParams()
+	
+
+func passShaderParams():
+	mesh.material.set_shader_parameter("index", 22.) #starts at 0
+	mesh.material.set_shader_parameter("totalShells", 31) #num of Shells-1
+	mesh.material.set_shader_parameter("shellHeight", .02) #total distance shell comes out in meters
+	mesh.material.set_shader_parameter("heightAttenuation", .65) #plug y = x^a into desmos to see a graph of this in action
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if update:
+		passShaderParams();
+		update = false
