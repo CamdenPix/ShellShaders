ba38230298a6e9f69c99e84221219e8f1e4145c9
Phoenix Pixel <camdengenta@gmail.com>
2023-11-05 04:23:38 -0600

    Code Cleanup and Final Pieces

---
+++ Scripts/minecraftBlockScripts/greentop6.gd
@@ -0,0 +1,21 @@
+@tool
+extends MeshInstance3D
+
+@export var update = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	passShaderParams()
+	
+
+func passShaderParams():
+	mesh.material.set_shader_parameter("index", 6.) #starts at 0
+	mesh.material.set_shader_parameter("totalShells", 15) #num of Shells-1
+	mesh.material.set_shader_parameter("shellHeight", .1) #total distance shell comes out in meters
+	mesh.material.set_shader_parameter("heightAttenuation", .85) #plug y = x^a into desmos to see a graph of this in action
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if update:
+		passShaderParams();
+		update = false
